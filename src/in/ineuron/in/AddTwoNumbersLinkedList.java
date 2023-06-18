5318c7f44dc4a5e442719ffa09b3ef2129a97fb6
Noor Mohammad <noormohammad5787@gmail.com>
2023-06-18 14:11:04 +0530

    First commit

---
+++ src/in/ineuron/in/AddTwoNumbersLinkedList.java
@@ -0,0 +1,95 @@
+package in.ineuron.in;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class AddTwoNumbersLinkedList {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        // Example 1: l1 = [2,4,3], l2 = [5,6,4]
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        AddTwoNumbersLinkedList solution = new AddTwoNumbersLinkedList();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        printLinkedList(result); // Output: [7, 0, 8]
+
+        // Example 2: l1 = [0], l2 = [0]
+        ListNode l3 = new ListNode(0);
+        ListNode l4 = new ListNode(0);
+        ListNode result2 = solution.addTwoNumbers(l3, l4);
+        printLinkedList(result2); // Output: [0]
+
+        // Example 3: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        ListNode l5 = new ListNode(9);
+        l5.next = new ListNode(9);
+        l5.next.next = new ListNode(9);
+        l5.next.next.next = new ListNode(9);
+        l5.next.next.next.next = new ListNode(9);
+        l5.next.next.next.next.next = new ListNode(9);
+        l5.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode l6 = new ListNode(9);
+        l6.next = new ListNode(9);
+        l6.next.next = new ListNode(9);
+        l6.next.next.next = new ListNode(9);
+
+        ListNode result3 = solution.addTwoNumbers(l5, l6);
+        printLinkedList(result3); // Output: [8, 9, 9, 9, 0, 0, 0, 1]
+    }
+
+    private static void printLinkedList(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val);
+            if (head.next != null) {
+                sb.append(" -> ");
+            }
+            head = head.next;
+        }
+        System.out.println(sb.toString());
+    }
+}
+
